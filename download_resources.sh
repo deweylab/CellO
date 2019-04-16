e1dc626054955c73659267a0afe35b5ce92edb96
Matthew Bernstein <berniematt18@gmail.com>
2019-04-16 10:57:53 -0500

    Fixed typo in download script

--- download_resources.sh
+++ download_resources.sh
@@ -1,5 +1,5 @@
 curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources/clr.pickle > ./cellpredict/resources/clr.pickle
 curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources/tpr.pickle > ./cellpredict/resources/tpr.pickle
 curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources/ir.pickle > ./cellpredict/resources/ir.pickle
-curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources/kallisto_reference.hg38_v27 > ./cellpredict/respirces/kallisto_reference.hg38_v27
+curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources/kallisto_reference.hg38_v27 > ./cellpredict/resources/kallisto_reference.hg38_v27
 curl http://deweylab.biostat.wisc.edu/cell_type_classification/CL.18-11-13.obo > './onto_lib/obo/CL.18-11-13.obo'
