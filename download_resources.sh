065e28ba1a2e330fc4e84bb841dcaa80380b4a1b
Matthew Bernstein <berniematt18@gmail.com>
2019-04-15 19:18:24 -0500

    Download same ontology file used to train classifiers

--- download_resources.sh
+++ download_resources.sh
@@ -2,3 +2,4 @@ curl https://uwmadison.box.com/s/0kuk0xufu2nzu027conbjs1hikbp5fe5 > ./cellpredic
 curl https://uwmadison.box.com/s/k7i0gx06ys3q15vt34vz70ncfrnpc7xw > ./cellpredict/resources/tpr.pickle
 curl https://uwmadison.box.com/s/2hf6agryrqbhgmm71p6sq9l3gd3fepkp > ./cellpredict/resources/ir.pickle
 curl https://uwmadison.box.com/s/xpqxf9ik775dmt3qoeczqa0xre2zlle7 > ./cellpredict/respirces/kallisto_reference.hg38_v27
+curl https://uwmadison.box.com/s/ehesu3yak96n1uuoc0qd0atn4t3x4ta9 > './onto_lib/obo/CL.18-11-13.obo'
