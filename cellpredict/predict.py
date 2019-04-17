0452236292e19717a5baed37df5725d9919d266c
Matthew Bernstein <berniematt18@gmail.com>
2019-04-17 11:01:30 -0500

    Ability to output cell type names rather than ontology ids

--- cellpredict/predict.py
+++ cellpredict/predict.py
@@ -10,6 +10,8 @@ import pkg_resources as pr
 import pandas as pd
 from pandas import DataFrame
 import cPickle
+import onto_lib
+from onto_lib import load_ontology
 
 from machine_learning import learners
 
@@ -34,12 +36,14 @@ def main():
     #parser.add_option("-a", "--a_descrip", action="store_true", help="This is a flat")
     parser.add_option("-a", "--algo", help="Hierarchical classification algorithm to apply. Must be one of: 'IR' - Isotonic regression, 'CLR' - cascaded logistic regression, 'TPR' - True Path Rule")
     parser.add_option("-s", "--single_cell", action="store_true", help="Perform predictions on single-cell data")
+    parser.add_option("-r", "--human_readable_labels", action="store_true", help="Output cell type names rather than Cell Ontology term id's.")
     (options, args) = parser.parse_args()
 
     queries_f = args[0]
     algo = 'IR'
     if options.algo:
         algo = options.algo
+    by_term_name = options.human_readable_labels
 
     # Set the prediction threshold to achieve 0.9 precision
     if options.single_cell:
@@ -54,11 +58,17 @@ def main():
             for l in f
         ]
 
-    df, df_scores = predict(queries, algo, thresh)
+    df, df_scores = predict(queries, algo, thresh, by_term_name)
     df.to_csv('predictions.tsv', sep='\t')
     df_scores.to_csv('prediction_scores.tsv', sep='\t')
 
-def predict(queries, algo, thresh):
+def predict(queries, algo, thresh, by_term_name=False):
+
+    if by_term_name:
+        # TODO 17 is the id for the ontology we want, but
+        # this is a bit messy
+        og = load_ontology.load('17')[0]
+    
     if algo == 'IR':
         model_f = pr.resource_filename(
             resource_package, join("resources", "ir.pickle")
@@ -83,7 +93,6 @@ def predict(queries, algo, thresh):
     ]
 
     label_order = label_to_confs[0].keys()
-
     da = [
         (int(label_to_conf[k] > thresh) for k in label_order)
         for label_to_conf in label_to_confs
@@ -92,8 +101,24 @@ def predict(queries, algo, thresh):
         (label_to_conf[k] for k in label_order)
         for label_to_conf in label_to_confs
     ]
-    df = DataFrame(data=da, columns=label_order)
-    df_scores = DataFrame(data=da_scores, columns=label_order)
+    if by_term_name:
+        df = DataFrame(
+            data=da, 
+            columns=[
+                og.id_to_term[x].name 
+                for x in label_order
+            ]
+        )
+        df_scores = DataFrame(
+            data=da_scores, 
+            columns=[
+                og.id_to_term[x].name 
+                for x in label_order
+            ]
+        )
+    else:
+        df = DataFrame(data=da, columns=label_order)
+        df_scores = DataFrame(data=da_scores, columns=label_order)
     return df, df_scores
 
 
