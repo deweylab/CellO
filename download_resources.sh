10fdd100fb7d0c128d30a3f66ee9c97c1692a753
Matthew Bernstein <berniematt18@gmail.com>
2020-06-29 13:34:34 -0500

    Fixed bug in download resources script

--- download_resources.sh
+++ download_resources.sh
@@ -2,5 +2,6 @@
 # Download the pre-trained models and other data, such as gene-symbol mappings, for running CellO
 ########################################################################################################
 
-curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources.tar.gz > resource.tar.gz
+curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources.tar.gz > resources.tar.gz
 tar -zxf resources.tar.gz
+rm resources.tar.gz
