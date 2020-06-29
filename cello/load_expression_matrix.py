c5e1aff9b71e005874f1bc2766bb82314b77f193
Matthew Bernstein <berniematt18@gmail.com>
2020-06-29 12:28:44 -0500

    Added data-loading scripts

---
+++ cello/load_expression_matrix.py
@@ -0,0 +1,122 @@
+######################################################################
+#   Load a gene expression matrix
+######################################################################
+
+from os.path import join
+import pandas as pd
+from anndata import AnnData
+import h5py
+from scipy.io import mmread
+import numpy as np
+
+def load_data(data_loc, data_type, rows_cells=True):
+    if data_type == '10x':
+        ad = _read_10x(data_loc)
+    elif data_type == 'TSV':
+        ad = _read_csv(
+            data_loc,
+            '\t',
+            rows_cells
+        )
+    elif data_type == 'CSV':
+        ad = _read_csv(
+            data_loc,
+            ',',
+            options.rows_cells
+        )
+    elif data_type == 'HDF5':
+        ad = _read_hdf5(
+            data_loc,
+            options.hdf5_expr_key,
+            options.hdf5_cells_key,
+            options.hdf5_genes_key
+        )
+    return ad
+
+def _read_10x(data_loc):
+    # Load genes 
+    genes_f = join(data_loc, 'genes.tsv')
+    with open(genes_f, 'r') as f:
+        genes = [
+            l.split()[0].strip()
+            for l in f
+        ]
+
+    # Load barcodes
+    barcodes_f = join(data_loc, 'barcodes.tsv')
+    barcodes = pd.read_csv(
+        barcodes_f,
+        header=None,
+        index_col=0
+    ).index
+
+    # Load counts
+    expr_f = join(data_loc, 'matrix.mtx')
+    with open(expr_f, 'rb') as f:
+        X = mmread(f).todense().T
+
+    # Build AnnData object
+    ad = AnnData(
+        X=X,
+        obs=pd.DataFrame(
+            data=barcodes,
+            index=barcodes
+        ),
+        var=pd.DataFrame(
+            data=genes,
+            index=genes
+        )
+    )
+    return ad
+
+def _read_csv(data_loc, sep, rows_cells):
+    df = pd.read_csv(
+        data_loc,
+        sep=sep,
+        index_col=0
+    )
+    if not rows_cells:
+        df = df.transpose()
+    ad = AnnData(
+        X=np.array(df),
+        obs=pd.DataFrame(
+            index=df.index,
+            data=df.index
+        ),
+        var=pd.DataFrame(
+            index=df.columns,
+            data=df.columns
+        )
+    )
+    return ad
+
+
+def _read_hdf5(
+        data_loc,
+        expr_key,
+        cells_key,
+        genes_key
+    ):
+    with h5py.File(data_loc, 'r') as f:
+        cells = [
+            str(x)[2:-1]
+            for x in f[expr_key][:]
+        ]
+        genes = [
+            str(x)[2:-1]
+            for x in f[genes_key][:]
+        ]  
+        X = np.array(f[expr_key][:])
+    ad = AnnData(
+        X=X,
+        obs=pd.DataFrame(
+            index=cells,
+            data=cells
+        ),
+        var=pd.DataFrame(
+            index=genes,
+            data=genes
+        )
+    )
+    return ad
+
---
+++ cello/load_training_data.py
@@ -0,0 +1,172 @@
+import json
+from os.path import join
+import h5py
+import sys
+from collections import defaultdict
+import pkg_resources as pr
+
+resource_package = __name__
+
+from . import the_ontology
+from graph_lib.graph import DirectedAcyclicGraph
+
+DATA_DIR = pr.resource_filename(
+    resource_package,
+    join("resources", "training_set")
+) 
+
+def load(features):
+    labels_f = join(DATA_DIR, 'labels.json')
+    studys_f = join(DATA_DIR, 'experiment_to_study.json')
+    tags_f = join(DATA_DIR, 'experiment_to_tags.json')
+    expr_matrix_f = join(DATA_DIR, '{}.h5'.format(features))
+
+    # Load the ontology
+    og = the_ontology.the_ontology()
+
+    # Load labels and labels-graph
+    with open(labels_f, 'r') as f:
+        labels_data = json.load(f)
+        source_to_targets= labels_data['label_graph']
+        exp_to_labels = labels_data['labels']
+    label_graph = DirectedAcyclicGraph(source_to_targets)
+
+    # Map each ontology term label to its human-readable
+    # name
+    label_to_name = {
+        label: og.id_to_term[label].name
+        for label in source_to_targets.keys()
+    }
+
+    # Map each experiment to its most-specific labels
+    exp_to_ms_labels = {
+        exp: label_graph.most_specific_nodes(labels)
+        for exp, labels in exp_to_labels.items()
+    }
+    
+    # Load study metadata
+    with open(studys_f, 'r') as f:
+        exp_to_study = json.load(f)
+    study_to_exps = defaultdict(lambda: set())
+    for exp, study in exp_to_study.items():
+        study_to_exps[study].add(exp)
+    study_to_exps = dict(study_to_exps)
+
+    # Load technical tags
+    with open(tags_f, 'r') as f:
+        exp_to_tags = json.load(f)
+
+    # Load the data matrix
+    print('Loading expression data from {}...'.format(expr_matrix_f))
+    with h5py.File(expr_matrix_f, 'r') as f:
+        the_exps = [
+            str(x)[2:-1]
+            for x in f['experiment'][:]
+        ]
+        gene_ids = [
+            str(x)[2:-1]
+            for x in f['gene_id'][:]
+        ]
+        data_matrix = f['expression'][:]
+    print('Loaded matrix of shape {}'.format(data_matrix.shape))
+    print('done.')
+
+    # Map each experiment to its index
+    exp_to_index = {
+        exp: ind
+        for ind, exp in enumerate(the_exps)
+    }
+    return (
+        og,
+        label_graph,
+        label_to_name,
+        the_exps,
+        exp_to_index,
+        exp_to_labels,
+        exp_to_tags,
+        exp_to_study,
+        study_to_exps,
+        exp_to_ms_labels,
+        data_matrix,
+        gene_ids
+    )
+
+
+def load_sparse_dataset():
+    labels_f = join(data_dir, 'labels.json')
+    studys_f = join(data_dir, 'experiment_to_study.json')
+    tags_f = join(data_dir, 'experiment_to_tags.json')
+    expr_matrix_f = join(data_dir, '{}.h5'.format(features))
+
+    # Load the ontology
+    og = the_ontology.the_ontology()
+
+    # Load labels and labels-graph
+    with open(labels_f, 'r') as f:
+        labels_data = json.load(f)
+        source_to_targets= labels_data['label_graph']
+        exp_to_labels = labels_data['labels']
+    label_graph = DirectedAcyclicGraph(source_to_targets)
+
+    # Map each ontology term label to its human-readable
+    # name
+    label_to_name = {
+        label: og.id_to_term[label].name
+        for label in source_to_targets.keys()
+    }
+
+    # Map each experiment to its most-specific labels
+    exp_to_ms_labels = {
+        exp: label_graph.most_specific_nodes(labels)
+        for exp, labels in exp_to_labels.items()
+    }
+
+    # Load study metadata
+    with open(studys_f, 'r') as f:
+        exp_to_study = json.load(f)
+    study_to_exps = defaultdict(lambda: set())
+    for exp, study in exp_to_study.items():
+        study_to_exps[study].add(exp)
+    study_to_exps = dict(study_to_exps)
+
+    # Load technical tags
+    with open(tags_f, 'r') as f:
+        exp_to_tags = json.load(f)
+
+    # Load the data matrix
+    print('Loading expression data from {}...'.format(expr_matrix_f))
+    with h5py.File(expr_matrix_f, 'r') as f:
+        the_exps = [
+            str(x)
+            for x in f['experiment'][:]
+        ]
+        gene_ids = [
+            str(x)
+            for x in f['gene_id'][:]
+        ]
+        data_matrix = f['expression'][:]
+    print('Loaded matrix of shape {}.'.format(data_matrix.shape))
+    print('done.')
+
+    # Map each experiment to its index
+    exp_to_index = {
+        exp: ind
+        for ind, exp in enumerate(the_exps)
+    }
+    return (
+        og,
+        label_graph,
+        label_to_name,
+        the_exps,
+        exp_to_index,
+        exp_to_labels,
+        exp_to_tags,
+        exp_to_study,
+        study_to_exps,
+        exp_to_ms_labels,
+        data_matrix,
+        gene_ids
+    )
+
+if __name__ == '__main__':
+    main()
