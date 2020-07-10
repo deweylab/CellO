ee11ff61262b436d1c96d1136a01038720a3d3ec
Matthew Bernstein <berniematt18@gmail.com>
2020-07-10 12:28:14 -0500

    Versioned resources

--- download_resources.sh
+++ download_resources.sh
@@ -2,6 +2,6 @@
 # Download the pre-trained models and other data, such as gene-symbol mappings, for running CellO
 ########################################################################################################
 
-curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources.tar.gz > resources.tar.gz
-tar -zxf resources.tar.gz
-rm resources.tar.gz
+curl http://deweylab.biostat.wisc.edu/cell_type_classification/resources_v1.0.0.tar.gz > resources_v1.0.0.tar.gz
+tar -zxf resources_v1.0.0.tar.gz
+rm resources_v1.0.0.tar.gz
