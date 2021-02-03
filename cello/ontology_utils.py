ad36e8c9dddbe03036e50a0eddf24c9070874e4f
Matthew Bernstein <berniematt18@gmail.com>
2021-02-03 11:05:22 -0600

    Add module for ontology utilities

---
+++ cello/ontology_utils.py
@@ -0,0 +1,9 @@
+from . import the_ontology
+
+CELL_ONTOLOGY = the_ontology.the_ontology()
+
+def get_term_name(term_id):
+    try:
+        return CELL_ONTOLOGY.id_to_term[term_id].name
+    except KeyError:
+        return None
