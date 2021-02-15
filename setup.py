d365515def21821782ec97ed9f995336a9472d10
Matthew Bernstein <matthewbernstein@Matthews-MacBook-Pro-2.local>
2021-02-15 10:48:41 -0600

    Removed strictness of scipy version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = [
     "quadprog>=0.1.6",
     "numpy>=1.17.1",
     "scikit-learn>=0.22.2.post1",
-    "scipy==1.3.1",
+    "scipy>=1.3.1",
     "pandas>=0.23.4",
     "dill>=0.3.1.1",
     "h5py>=2.10.0",
